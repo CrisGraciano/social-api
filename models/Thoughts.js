a109084e954d7c7b202369b3de2a7a8f0519fcad
CrisGraciano <cristobalgraciano98@gmail.com>
2022-11-16 18:58:57 -0600

    thoughts model created

--- models/Thoughts.js
+++ models/Thoughts.js
@@ -1,7 +1,25 @@
 // require dependencies
 const { Schema, model } = require('mongoose');
 
-const ThoughtsSchema = new Schema({});
+const ThoughtsSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: [ReactionsSchema]
+    }
+);
 
 const Thoughts = model('Thoughts', ThoughtsSchema);
 
