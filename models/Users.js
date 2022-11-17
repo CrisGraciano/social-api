bc244f375456dd6fb0cbb7a044a4003bf4f836c6
CrisGraciano <cristobalgraciano98@gmail.com>
2022-11-16 18:46:31 -0600

    basic users model done

--- models/Users.js
+++ models/Users.js
@@ -1,6 +1,29 @@
 const { Schema, model } = require('mongoose');
 
-const UsersSchema = new Schema({});
+const UsersSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match:  //maybe use regex here??
+        },
+        thoughts: [{
+            type: Schema.Types.ObjectId,
+            ref: 'Thoughts'
+        }],
+        friends:[{
+            type: Schema.Types.ObjectId,
+            ref: 'Users',
+        }]
+    }
+);
 
 const Users = model('Users', UsersSchema);
 
