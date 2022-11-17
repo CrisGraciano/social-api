3804974c4908ed331b48e7d5914f05ace4acbdb8
CrisGraciano <cristobalgraciano98@gmail.com>
2022-11-16 18:25:12 -0600

    server is working successfully

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const mongoose = require('mongoose');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.static('public'));
 
-app.use(require('./routes'));
+// app.use(require('./routes'));
 
 mongoose.connect(process.envMONGODB_URI || 'mongodb://127.0.0.1:27017/social-api', {
     useNewUrlParser: true,
@@ -17,4 +17,4 @@ mongoose.connect(process.envMONGODB_URI || 'mongodb://127.0.0.1:27017/social-api
 
 mongoose.set('debug', true);
 
-app.listen(POrt, () => console.log(`Connected on localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Connected on localhost:${PORT}`));
